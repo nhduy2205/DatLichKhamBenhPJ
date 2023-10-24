22ff101f8e9d878d9dd86c98fd643fc6c37b79f2
unknown <nhduy220597@gmail.com>
2023-10-24 22:39:07 +0700

    api đăng nhập, xac thuc

--- models/BenhNhan.js
+++ models/BenhNhan.js
@@ -1,35 +1,13 @@
 const mongoose = require('mongoose');
 
 const BenhNhanSchema = new mongoose.Schema({
-    hoten: {
-        type: String,
-        required: true
-    },
     sodienthoai: {
         type: String,
         required: true
     },
-    ngaysinh: {
-        type: Date,
-        default: Date.now
-    },
-    gioitinh: {
-        type: Number,
-        required: true
-    },
-    bhyt: [{
-        mathe: {
-            type: String
-        },
-        thoihanthetu: {
-            type: Date,
-            default: null
-        },
-        thoihantheden: {
-            type: Date,
-            default: null
-        }
-    }]
+    maxacthuc: {
+        type: String
+    }
 });
 
 module.exports = BenhNhan = mongoose.model('benhnhan', BenhNhanSchema);
--- routes/api/benhnhan.js
+++ routes/api/benhnhan.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => res.send('benhnhan route'));
 // @desc TEST route
 // @access Public
 
-router.post('/' , 
+router.post('/themthongtin' , 
 [check('hoten', 'hoten is required').not().isEmpty(),
 check('sodienthoai', 'sodienthoai is required').not().isEmpty(),
 check('gioitinh', 'gioitinh is required').not().isEmpty()],
@@ -42,5 +42,66 @@ async (req, res) => {
         res.status(500).json({errors: [{msg: err.msg}]});
     }
 });
+
+
+// api đăng nhập => xác thực bằng số điện thoại
+
+router.post('/dangnhap' , 
+[check('sodienthoai', 'sodienthoai is required').not().isEmpty()],
+async (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {sodienthoai} = req.body;
+    const maxacthuc = Math.floor(Math.random() * 1000000);
+    try {
+        let benhnhan = await BenhNhan.findOne({sodienthoai});
+        if(benhnhan){
+            // nếu bệnh nhân đã đăng ký sdt rồi thì đổi mã xác thực và lưu lại
+            let benhnhan_update = await BenhNhan.findById(benhnhan.id);
+            benhnhan_update.maxacthuc = maxacthuc;
+            await benhnhan_update.save();
+            benhnhan = benhnhan_update;
+            res.json({ benhnhan });
+        }else {
+            benhnhan = new BenhNhan({
+                sodienthoai,
+                maxacthuc: String(maxacthuc)
+            });
+    
+            await benhnhan.save();
+            res.json({ benhnhan });
+        }
+    }catch(err){
+        res.status(500).json({errors: [{msg: err.msg}]});
+    }
+});
+
+// api đăng nhập => xác thực bằng số điện thoại
+
+router.post('/xacthuc/:id' , 
+[check('maxacthuc', 'maxacthuc is required').not().isEmpty()],
+async (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array()});
+    }
+    const {maxacthuc} = req.body;
+
+    try {
+        let benhnhan = await BenhNhan.findById(req.params.id);
+        if(benhnhan && benhnhan.maxacthuc == maxacthuc){            
+           return res.status(200).json({ benhnhan });
+        } 
+        return res.status(400).send("Mã xác thực không đúng!");
+
+    }catch(err){
+        res.status(500).json({errors: [{msg: err.msg}]});
+    }
+});
+
 
 module.exports = router;
